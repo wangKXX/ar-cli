6caa989fe584758b9114b26a4bef379d1259d6a1
kai.wang <kai.wang@baozun.com>
2021-04-14 16:11:13 +0800

    [feat]fix

--- template/vue-template.js
+++ template/vue-template.js
@@ -7,7 +7,7 @@ const utils = require("../utils");
 module.exports = (appName, isInstall) => {
   spinner.start("project init....");
   download(
-    "https://github.com/wangKXX/template.git",
+    "direct:https://github.com/wangKXX/template.git",
     appName,
     { clone: true },
     err => {
